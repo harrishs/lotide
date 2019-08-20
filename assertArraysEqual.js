6ee8098dde6374777bf738d199b8477fb5d3faaa
Harrish S <harrish.s@hotmail.ca>
2019-08-20 17:04:33 +0000

    Check if arrays are equal and assert using function

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+function eqArrays(arr1, arr2){
+  if (arr1.length !== arr2.length){
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]){
+      return false;
+    }
+  }
+  return true;
+}
+
+function assertArraysEqual(arr1, arr2){
+  let bool = eqArrays(arr1, arr2);
+  if (bool === true){
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+  }
+  else{
+    console.log(`🔴 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+}
+
+console.log(assertArraysEqual([],[0]));
+console.log(assertArraysEqual([1,2,"3"], [1,2,3]));
+console.log(assertArraysEqual([1],[1,2,3]));
+console.log(assertArraysEqual([1,2,3],[1,2,3]));
