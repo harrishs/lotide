3f8ee925a44982207115290efa4dc217eca54790
Harrish S <harrish.s@hotmail.ca>
2019-08-20 22:25:21 +0000

    Count letters in given object/ string

---
+++ countLetters.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(allItems){
+  const counts = {}
+  for (const element of allItems){
+    counts[element] = element.length;
+  }
+  return counts;
+}
