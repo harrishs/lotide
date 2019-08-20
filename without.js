0aaeb89529cb92c1e4936729c4f365bc9238730d
Harrish S <harrish.s@hotmail.ca>
2019-08-20 18:20:25 +0000

    remove values provided in an array from another array

---
+++ without.js
@@ -0,0 +1,38 @@
+function eqArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function assertArraysEqual(arr1, arr2) {
+  let bool = eqArrays(arr1, arr2);
+  if (bool === true) {
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+}
+
+function without(array, remove) {
+  let newArr = [];
+  for (let i = 0; i < array.length; i++) {
+    for (let element of remove) {
+      if (element === array[i]) {
+        break;
+      } else if (element !== remove[remove.length - 1]) {
+        continue;
+      } else {
+        newArr.push(array[i]);
+      }
+    }
+  }
+  return newArr;
+}
+
+
