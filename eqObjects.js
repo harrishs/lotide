f5b9ebaab0ae84bd6fd541b17e90b1d352f8bbef
Harrish S <harrish.s@hotmail.ca>
2019-08-21 17:10:14 +0000

    check if objects are equal

---
+++ eqObjects.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function eqArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const eqObjects = function(object1, object2) {
+  let keys1 = Object.keys(object1);
+  let keys2 = Object.keys(object2);
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+  for (let element of keys1) {
+    if (Array.isArray(object1[element]) && Array.isArray(object2[element])) {
+      return eqArrays(object1[element],object2[element]);
+    }
+    if (object1[element] !== object2[element]) {
+      return false;
+    }
+  }
+  return true;
+};
