2f9e82aab09105242ff6d5234409a92d6653377b
Harrish S <harrish.s@hotmail.ca>
2019-08-21 17:32:40 +0000

    Print message showing whether 2 given objects are equal

---
+++ assertObjectsEqual.js
@@ -0,0 +1,38 @@
+function eqArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const eqObjects = function(object1, object2) {
+  let keys1 = Object.keys(object1);
+  let keys2 = Object.keys(object2);
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+  for (let element of keys1) {
+    if (Array.isArray(object1[element]) && Array.isArray(object2[element])) {
+      return eqArrays(object1[element],object2[element]);
+    }
+    if (object1[element] !== object2[element]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  let bool = eqObjects(actual, expected);
+  if (bool === true) {
+    console.log(`✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
