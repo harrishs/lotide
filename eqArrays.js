563e53fd8c6c8c4b312490dd8afbd7da835f8cc4
Harrish S <harrish.s@hotmail.ca>
2019-08-23 18:36:37 +0000

    updated to work with nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -7,13 +7,35 @@ const assertEqual = function(actual, expected) {
 };
 
 function eqArrays(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
+
+  // console.log("received:", arr1, "!!!!", arr2)
+
+  // console.log("length:", arr1.length, arr2.length)
+  if (arr1.length != arr2.length) {
+    // console.log("length:", arr1, arr2)
     return false;
   }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
+    for (let i = 0; i < arr1.length; i++) {
+      // console.log("processing:", arr1[i], arr2[i])
+      if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+        if (arr1[i].length != arr2[i].length) {
+          return false;
+        }
+       eqArrays(arr1[i], arr2[i]);
+       continue;
+      } 
+      if (arr1[i] === arr2[i]) {
+        return false;
+      } 
     }
-  }
-  return true;
+    return true;
 }
+
+assertEqual((eqArrays([
+  [2, 3], [[[[1]]], [[[1]]]]
+],
+   [
+     [2, 3], 
+     [[[[1]]],[[[1]]]]
+    ])), true);
+console.log(eqArrays([[2, 3], [4,]], [[2, 3, [[[1]]]], [4, 5]]));
